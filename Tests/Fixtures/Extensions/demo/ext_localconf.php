00cbeed964505cd21980a8988831df6e8d22f923
Daniel Goerz <daniel.goerz@b13.de>
2018-12-20 09:29:41 +0100

    [BUGFIX] Assure a source attribute is always included in created files and labels

--- Tests/Fixtures/Extensions/demo/ext_localconf.php
+++ Tests/Fixtures/Extensions/demo/ext_localconf.php
@@ -1,4 +1,4 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['l10n_translator']['availableLanguages'] = 'de';
-$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['l10n_translator']['availableTranslationFiles'] = 'demo/Resources/Private/Language/locallang.xlf,demo/Resources/Private/Language/test.xml';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['l10n_translator']['availableLanguages'] = 'de,default,fr,it';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['l10n_translator']['availableL10nFiles'] = 'demo/Resources/Private/Language/locallang.xlf,demo/Resources/Private/Language/test.xml';
