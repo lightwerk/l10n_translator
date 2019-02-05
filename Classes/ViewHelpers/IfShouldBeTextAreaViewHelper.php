1bea1808d7c90ddd522afcdd6b794d0072225fdc
Daniel Goerz <daniel.goerz@b13.de>
2019-02-05 15:47:59 +0100

    [TASK] Streamline PHP doc block

--- Classes/ViewHelpers/IfShouldBeTextAreaViewHelper.php
+++ Classes/ViewHelpers/IfShouldBeTextAreaViewHelper.php
@@ -2,17 +2,12 @@
 
 namespace Lightwerk\L10nTranslator\ViewHelpers;
 
-/**
- * This file is part of the TYPO3 CMS project.
+/*
+ * This file is part of TYPO3 CMS-based extension l10n_translator by b13.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
  * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
  */
 
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
